7ceaaf8fc0a2685954c392226d83eaf05fe99f9b
Alejandro Guespud <alejo.habbacuc@gmail.com>
2021-08-06 23:58:22 -0500

    cambios practica

--- JS/nav.js
+++ JS/nav.js
@@ -2,9 +2,9 @@
     $(".toggle").on("click", function() {
         if ($(".item").hasClass("active")) {
             $(".item").removeClass("active");
-            $(this).find("a").html("<i class='fas fa-bars'></i>");
+            $(this).find("a").html("<img src='../ASSETS/icons/Column Icon.png'>");
         } else {
             $(".item").addClass("active");
-            $(this).find("a").html("<i class='fas fa-times'></i>");
+            $(this).find("a").html("<img src='../ASSETS/icons/Column Icon.png'>");
         }
     });
